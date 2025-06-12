dc3f6f1e8c999371a6b65c91cea962f78914a17a
MSRInvestigator <msrecoveriesinvestigations@gmail.com>
2025-06-12 17:48:34 -0600

    Add files via upload

--- models.py
+++ models.py
@@ -1,30 +1,19 @@
 
 import mysql.connector
+from passlib.hash import phpass
 
-def add_log_entry(username, start_time, end_time, total_hours):
+def get_wp_user(username, password):
     conn = mysql.connector.connect(
-        host="localhost",
-        user="i1816040_wp2",
-        password="H.qVNOLd8O39IKmlQFa50",
-        database="i1816040_wp2"
-    )
-    cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO wp_logs (username, start_time, end_time, total_hours)
-        VALUES (%s, %s, %s, %s)
-    """, (username, start_time, end_time, total_hours))
-    conn.commit()
-    conn.close()
-
-def get_logs_by_user(username):
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="i1816040_wp2",
-        password="H.qVNOLd8O39IKmlQFa50",
-        database="i1816040_wp2"
+        host="msradminwp1.db.330.hostedresource.com",  # External MySQL hostname
+        user="i1816040_wp2",                          # DB username
+        password="H.qVNOLd8O39IKmlQFa50",             # DB password
+        database="i1816040_wp2"                       # DB name
     )
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM wp_logs WHERE username = %s", (username,))
-    logs = cursor.fetchall()
+    cursor.execute("SELECT * FROM wp_users WHERE user_login = %s", (username,))
+    user = cursor.fetchone()
     conn.close()
-    return logs
+
+    if user and phpass.verify(password, user['user_pass']):
+        return user
+    return None
